5638f8c8c3ad6d515f4c08dd08751037cb3a7139
rodrigo C. <Rodrigoconcham@gmail.com>
2025-11-10 16:27:30 -0300

    pkg 2

--- main.go
+++ main.go
@@ -3,8 +3,14 @@ package main
 import (
 	"aprende-golang/mathutil"
 	"fmt"
+	"math"
 )
 
 func main() {
 	fmt.Println(mathutil.Add(2, 3))
+
+	fmt.Println(mathutil.Resta(2, 3))
+
+	fmt.Println(math.Sqrt(144))
+
 }
--- mathutil/mathutil.go
+++ mathutil/mathutil.go
@@ -3,3 +3,7 @@ package mathutil
 func Add(x, y int) int {
 	return x + y
 }
+
+func Resta(x, y int) int {
+	return x - y
+}
