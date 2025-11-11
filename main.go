aa2aab52ab14070622043fed7baddd545b28bf49
rodrigo C. <Rodrigoconcham@gmail.com>
2025-11-11 12:38:26 -0300

    IF

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
@@ -15,8 +16,30 @@ func main() {
 	suma := add(2, 3)
 
 	fmt.Println(suma)
+
+	division, err := dividir(1, 1)
+
+	if err != nil {
+		fmt.Println("hay error")
+	}
+
+	fmt.Println(division)
+
+	if y:=10; y > 5 {
+        fmt.Println("y es mayor de 5")
+
+	}
 }
 
 func add(x, y int) int {
 	return x + y
+}
+
+func dividir(x, y int) (int, error) {
+	if y <= 0 {
+		return 0, errors.New("no puede ser zero")
+	}
+
+	return x / y, nil
+
 }
