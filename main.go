857b9142bf4e169b60f564dfd6c8b09e1835fd53
rodrigo C. <Rodrigoconcham@gmail.com>
2025-11-11 13:44:04 -0300

    prueba

--- main.go
+++ main.go
@@ -1,45 +1,21 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
-func decirHola() {
-	fmt.Println("Hola!")
-}
+
 
 func main() {
+x:=-1
+ if x > 0 {
+ 	fmt.Println("es positivo")
+ } else if x==0 {
+     fmt.Println("es zero")
+ } else {
+  fmt.Println("es negativo")
+ }
 
-	decirHola()
-
-	suma := add(2, 3)
-
-	fmt.Println(suma)
-
-	division, err := dividir(1, 1)
-
-	if err != nil {
-		fmt.Println("hay error")
-	}
-
-	fmt.Println(division)
-
-	if y:=10; y > 5 {
-        fmt.Println("y es mayor de 5")
-
-	}
+	
 }
 
-func add(x, y int) int {
-	return x + y
-}
-
-func dividir(x, y int) (int, error) {
-	if y <= 0 {
-		return 0, errors.New("no puede ser zero")
-	}
-
-	return x / y, nil
-
-}
