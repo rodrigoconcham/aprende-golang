186a24dd8f9526e56dbe3cb322fcfa85f0c81319
rodrigo C. <Rodrigoconcham@gmail.com>
2025-11-12 10:38:49 -0300

    swirch 1

--- main.go
+++ main.go
@@ -1,33 +1,15 @@
-   package main
-
+package main
 
 import "fmt"
 
-func decirAdios() string {
-    return "Adios"
-}
-
-
-
 func main() {
-
-	despedida :=decirAdios()
-   fmt.Println(despedida)
-
-  divided := divide(1,2)
-  fmt.Println(divided)
-   
-
-
-
-	 
-}
-
-func divide(a , b int ) int {
-
-  if b==0  {
-    return 0
-  }
-
-  return a / b 
+    day := 3
+    switch day {
+    case 1:
+        fmt.Println("Monday")
+    case 2:
+        fmt.Println("Tuesday")
+    default:
+        fmt.Println("Other day")
+    }
 }
