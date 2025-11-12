e8145ec3c8def4865b6c479881e22d4d64dd7624
rodrigo C. <Rodrigoconcham@gmail.com>
2025-11-12 11:44:19 -0300

    case

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import "fmt"
 
 func main() {
-    letter := "a"
-    switch letter {
-    case "a", "e", "i", "o", "u":
-        fmt.Println("Vocal")
+    number := 0
+    switch number {
+    case 0:
+          fallthrough
+    case 1:
+        fmt.Println("Number is either 0 or 1")
     default:
-        fmt.Println("Consonante")
+        fmt.Println("Number is something else")
     }
 }
