1c47f4387c158cfd9950dd1ac4cfbfc805d833e2
rodrigo C. <Rodrigoconcham@gmail.com>
2025-11-14 11:25:02 -0300

    select

--- main.go
+++ main.go
@@ -3,9 +3,16 @@ package main
 import "fmt"
 
 func main() {
-    ch := make(chan string, 2)
-    ch <- "Hello"
-    ch <- "World"
-    fmt.Println(<-ch) // Output: Hello
-    fmt.Println(<-ch) // Output: World
+    ch1 := make(chan string)
+    ch2 := make(chan string)
+    
+    go func() { ch1 <- "Channel 1" }()
+    go func() { ch2 <- "Channel 2" }()
+    
+    select {
+    case msg1 := <-ch1:
+        fmt.Println(msg1)
+    case msg2 := <-ch2:
+        fmt.Println(msg2)
+    }
 }
