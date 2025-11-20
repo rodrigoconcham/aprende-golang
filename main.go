f67cdddbd1d38e243837030990af113d4a21fdc0
Rodrigo C. <rodrigoconcham@gmail.com>
2025-11-20 11:59:20 -0300

    interface

--- main.go
+++ main.go
@@ -1,16 +1,13 @@
 package main
 
-import  (
-      "fmt"
-  )
+import "fmt"
 
+func imprimirCualquierCosa(v interface{}) {
+	fmt.Println(v)
+}
 
 func main() {
-    m := map[string]int{"manzana": 2, "banana": 3}
-    fmt.Println(m["manzana"]) // Output: 2
-  
-    for key, value := range m {
-        fmt.Printf("key es: %s; value es: %d", key, value)
-    }
-
+	imprimirCualquierCosa(42)
+	imprimirCualquierCosa("hola")
+	imprimirCualquierCosa(3.14)
 }
