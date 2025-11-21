dfe74807cd4675b900857fbf10b350832af3c6ea
Rodrigo C. <rodrigoconcham@gmail.com>
2025-11-21 11:54:35 -0300

    patrones

--- main.go
+++ main.go
@@ -1,10 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func fetchURL(url string) {
+	start := time.Now()
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("Error fetching %s: %v\n", url, err)
+		return
+	}
+	defer resp.Body.Close()
+
+	duracion := time.Since(start)
+	fmt.Printf("Fetched %s en %v - Status %s\n", url, duracion, resp.Status)
+}
 
 func main() {
-	x := 10
-	p := &x
-	fmt.Println(p)  // Salida: Dirección de memoria de x (ej: 0xc000014088)
-	fmt.Println(*p) // Salida: 10
+
+	urls := []string{
+		"https://rickandmortyapi.com/api/character/1",
+		"https://rickandmortyapi.com/api/character/2",
+		"https://rickandmortyapi.com/api/character/3",
+		"https://rickandmortyapi.com/api/character/4",
+	}
+
+	start := time.Now()
+
+	for _, url := range urls {
+		fetchURL(url)
+	}
+
+	duracionTotal := time.Since(start)
+	fmt.Printf("Tiempo total de la secuencia: %v\n", duracionTotal)
+
 }
